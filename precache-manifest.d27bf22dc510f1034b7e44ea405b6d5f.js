e2d6a38a9f795e2f783c006d8cd4f6b6b55a2f64
nicolej2122 <nicole.joseph2122@gmail.com>
2025-05-16 13:42:58 +0800

    Updates

---
+++ precache-manifest.d27bf22dc510f1034b7e44ea405b6d5f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48115ee6bdded82e3572089a10a4330f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2764879fb654bab9cfc5",
+    "url": "/static/css/2.2a46471a.chunk.css"
+  },
+  {
+    "revision": "6824f3df135b7fcf842c",
+    "url": "/static/css/main.c020480b.chunk.css"
+  },
+  {
+    "revision": "2764879fb654bab9cfc5",
+    "url": "/static/js/2.a6d0dc84.chunk.js"
+  },
+  {
+    "revision": "99fa1f833a603c8fde25549567beaaf3",
+    "url": "/static/js/2.a6d0dc84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6824f3df135b7fcf842c",
+    "url": "/static/js/main.2f9db0d3.chunk.js"
+  },
+  {
+    "revision": "8fb8177d277a0a6e238c",
+    "url": "/static/js/runtime-main.42801138.js"
+  }
+]);
